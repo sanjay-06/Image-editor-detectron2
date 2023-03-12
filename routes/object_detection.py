a61af15f3409539d1ff1cc40a631d434e7a028c4
Sanjay T <59244273+sanjay-06@users.noreply.github.com>
2023-03-13 01:48:26 +0530

    endpoint done for panoptic segmentation

--- routes/object_detection.py
+++ routes/object_detection.py
@@ -1,11 +1,14 @@
+# import some common libraries
+import os
+import cv2
+import json
+import random
+import numpy as np
+
 # Some basic setup:
 # Setup detectron2 logger
 from detectron2.utils.logger import setup_logger
 setup_logger()
-
-# import some common libraries
-import numpy as np
-import os, json, cv2, random
 
 # import some common detectron2 utilities
 from detectron2 import model_zoo
@@ -15,20 +18,23 @@ from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
-import cv2
 import matplotlib.pyplot as plt
 from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse
+from fastapi import File, UploadFile
+import shutil
+from PIL import Image
 
 object_detect=APIRouter()
 templates=Jinja2Templates(directory="html")
 
 class ObjectDetection:
     def __init__(self) -> None:
-        # Inference with a panoptic segmentation model
         self.cfg = get_cfg()
         self.cfg.merge_from_file(model_zoo.get_config_file("COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml"))
         self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml")
         self.predictor = None
+        self.image_name = None
     
     def predict(self, image):
         self.predictor = DefaultPredictor(self.cfg)
@@ -37,21 +43,37 @@ class ObjectDetection:
         out = v.draw_panoptic_seg_predictions(panoptic_seg.to("cpu"), segments_info)
         visual = cv2.cvtColor(out.get_image()[:, :, ::-1], cv2.COLOR_BGR2RGB)
         return visual
-# cv2_imshow(out.get_image()[:, :, ::-1])
 
 obj = ObjectDetection()
 
-@object_detect.get('/', response_class=HTMLResponse)
-def load(request: Request):
+@object_detect.get('/')
+def detect(request : Request):
     return templates.TemplateResponse("index.html",{"request":request})
 
-@object_detect.post('/{string}')
-def detect(request : Request, string):
-    print(string)
-    im = cv2.imread(string)
+@object_detect.get('/detect', response_class=HTMLResponse)
+def load(request: Request):
+    im = cv2.imread('html/'+obj.image_name)
     img = cv2.cvtColor(im, cv2.COLOR_BGR2RGB) 
     visual = obj.predict(img)
-    plt.imshow(visual)
-    plt.show()
-    return templates.TemplateResponse("index.html",{"request":request})
+    file_static_location = f"static/detect.jpg"
+    file_location = f"html/static/detect.jpg"
+    cv2.imwrite(file_location, visual)
+    return templates.TemplateResponse("show.html",{"request":request, "image": obj.image_name, "detect": file_static_location})
+
+@object_detect.post('/upload_file')
+async def handle_form(upload_file:UploadFile = File(...)):
+    filename=upload_file.filename
+    file_static_location = f"static/{filename}"
+    file_location = f"html/{file_static_location}"
+    with open(file_location, "wb") as buffer:
+        shutil.copyfileobj(upload_file.file, buffer)
+    
+    obj.image_name = file_static_location
+
+    return {"message":"success","statuscode":200}
+
+@object_detect.get('/show')
+async def show_image(request: Request):
+    print(obj.image_name)
+    return templates.TemplateResponse("show.html",{"request":request, "image": obj.image_name})
 
