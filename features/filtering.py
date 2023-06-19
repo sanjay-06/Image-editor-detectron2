3ef2552cfad406633144eb362e3edd8c38d4ed21
Sanjay T <59244273+sanjay-06@users.noreply.github.com>
2023-06-19 10:05:39 +0530

    debugger change

--- features/filtering.py
+++ features/filtering.py
@@ -10,7 +10,6 @@ class Filter:
     def perform_serpia_filter(self, img):
         sepia_kernel = np.array([[0.393, 0.769, 0.189], [0.349, 0.686, 0.168], [0.272, 0.534, 0.131]])
         sepia = cv2.transform(img, sepia_kernel)
-        print(sepia)
         return sepia
     
     def perform_vintage(self, img):
